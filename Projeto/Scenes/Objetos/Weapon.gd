319ac58ec257528709f81e9e74063191e27a7a90
pieraqua <pauloffxii@hotmail.com>
2024-01-29 16:37:34 -0300

    hitbox mostrando

--- Projeto/Scenes/Objetos/Weapon.gd
+++ Projeto/Scenes/Objetos/Weapon.gd
@@ -9,11 +9,13 @@ func _ready():
 func hit():
 	if($ReloadTimer.is_stopped()):
 		$AnimatedSprite2D.play()
+		$AnimatedSprite2D/Hitbox/HitboxPrincipal/HitboxIndicator.set_visible(true)
 		$ReloadTimer.start(RELOAD)
-		for body in $Hitbox.get_overlapping_bodies():
+		for body in $AnimatedSprite2D/Hitbox.get_overlapping_bodies():
 			if body.has_method("hit"):
 				body.hit(ATK)
 
 func _on_animation_finish():
 	$AnimatedSprite2D.stop()
+	$AnimatedSprite2D/Hitbox/HitboxPrincipal/HitboxIndicator.set_visible(false)
 	
