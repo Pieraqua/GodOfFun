affec989f5776b5d863f22467dc743aa641102e9
pieraqua <pauloffxii@hotmail.com>
2024-01-30 15:30:46 -0300

    Combo indicator

--- Projeto/Scenes/Scripts/UI.gd
+++ Projeto/Scenes/Scripts/UI.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 func _on_update_combo(current_combo : int):
-	get_node("/root/MainScene/UI/Container_pontos/Texto_combo").text = "Combo: " + str(int(current_combo))
+	$ContainerPontos/TexturaCombo/TextoCombo.text = str(int(current_combo))
 	
 func _on_update_points(current_points : int):
-	get_node("/root/MainScene/UI/Container_pontos/Texto_pontos").text = "Pontos: " + str(int(current_points))
+	$ContainerPontos/TextoPontos.text = "Pontos: " + str(int(current_points))
