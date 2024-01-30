7e8785f80e5f029678aa64889dd9c8275c311fbc
pieraqua <pauloffxii@hotmail.com>
2024-01-30 01:46:45 -0300

    Sistema de pontos funcionando

--- Projeto/Scenes/Scripts/Pontos.gd
+++ Projeto/Scenes/Scripts/Pontos.gd
@@ -4,7 +4,6 @@ const COMBO_MAX_TIME = 5
 const COMBO_MIN = 1
 
 var current_points = 0
-var combo_time = 0
 var character_alive = false
 var current_combo = COMBO_MIN
 
@@ -17,32 +16,27 @@ signal update_combo(int)
 func _ready():
 	_update_points(0)
 	_on_increase_combo(0)
-	pass # Replace with function body.
+	$ComboTimer.timeout.connect(_on_combo_over)
+	
+func _on_combo_over():
+	current_combo = COMBO_MIN
+	emit_signal("update_combo", current_combo)
+	
 	
 func on_game_start():
 	current_points = 0
-	combo_time = 0
 	current_combo = COMBO_MIN
 	_update_points(0)
 	_on_increase_combo(0)
 
-func _on_monster_killed(monster_type):
-	var combo_raise = 1
-	var pontos = 0
-	
-	if(monster_type <= globals.monsters.len()):
-		pontos = globals.monsters[monster_type][0]
-		combo_raise = globals.monsters[monster_type][1]
-	else:
-		print("_on_monster_killed: monster_type não registrado")
-	
-	_update_points(pontos)
+func on_monster_killed(points, combo_raise):
+	_update_points(points)
 	_on_increase_combo(combo_raise)
 	pass
 
 func _on_increase_combo(combo_raise : int):
 	current_combo += combo_raise
-	combo_time = COMBO_MAX_TIME
+	$ComboTimer.start(COMBO_MAX_TIME)
 	emit_signal("update_combo", current_combo)
 
 func _update_points(points : float):
@@ -53,9 +47,6 @@ func _update_points(points : float):
 func _process(delta):
 	if character_alive:
 		_update_points(current_combo * delta)
-		combo_time -= delta
-		if combo_time == 0:
-			current_combo = COMBO_MIN
 	pass
 
 func _on_map_loaded():
